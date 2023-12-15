7c3137a31b4b083ea1506999635df76986cad9dc
Dattobias <152405844+datto2530@users.noreply.github.com>
2023-12-15 13:43:18 +0700

    da xong

---
+++ ConsoleApp1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System;
+
+
+namespace ConsoleApp1
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int number;
+            Console.WriteLine("Nhap 1 so bat ki: ");
+            number = int.Parse(Console.ReadLine());
+            if (number < 2)
+                Console.WriteLine(number + " khong phai la so nguyen to");
+            else
+            {
+                int i = 2;
+                bool check = true;
+                while (i <= Math.Sqrt(number))
+                {
+                    if (number % i == 0)
+                    {
+                        check = false;
+                        break;
+                    }
+                    i++;
+                }
+                if (check)
+                {
+                    Console.WriteLine(number + " la mot so nguyen to");
+                }
+                else
+                {
+                    Console.WriteLine(number + " khong phai la so nguyen to");
+                }
+            }
+            Console.ReadKey();
+        }
+    }
+}
